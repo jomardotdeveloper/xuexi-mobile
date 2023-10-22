8b0473970f8223d296ce2006373baee5ad950f00
jomardotdeveloper <jomar.developer@gmail.com>
2023-10-22 19:42:51 +0800

    ASD

--- App.js
+++ App.js
@@ -1,43 +1,70 @@
-import React, { useRef } from 'react';
-import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { BackHandler } from 'react-native';
 import { WebView } from 'react-native-webview';
+ 
 
-export default function App() {
+const App = () => {
   const webViewRef = useRef(null);
+  useEffect(() => {
+    // const Tts = new Tts();
+    // Tts.setDefaultLanguage('en-IE');
+    // Tts.addEventListener('tts-start', event => console.log('start', event));
+    // Tts.addEventListener('tts-finish', event => console.log('finish', event));
+    // Tts.addEventListener('tts-cancel', event => console.log('cancel', event));
+    // Tts.stop();
+    // Tts.speak('Hello, world!');
+    console.log("TEST");
+    // console.log(Tts);
 
-  const goBack = () => {
-    if (webViewRef.current) {
-      webViewRef.current.goBack();
-    }
+    const backAction = () => {
+      console.log("TEST");
+      if (webViewRef.current) {
+        // Check if WebView can go back
+        webViewRef.current.goBack();
+        return true;
+      }
+
+      // If WebView cannot go back, prevent the app from exiting
+      return false;
+    };
+
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
+
+    // Invoke getPusher() function when the component is mounted
+    // Clean up the event listener and Pusher connection when the component unmounts
+    return () => {
+      backHandler.remove();
+    };
+  }, []); // Empty dependency array ensures that the effect runs once after the initial render
+
+
+  const onShouldStartLoadWithRequest = (event) => {
+    // This function will be called when the WebView is about to load a new URL
+    // You can use this to prevent certain URLs from loading if needed
+
+    // In this example, we allow all URLs to load
+    return true;
+  };
+
+  const handleMessage = (event) => {
+    const messageFromWeb = event.nativeEvent.data;
+    // Handle the message received from the web content
+    // console.table([messageFromWeb]);
+    console.log(event);
+    // console.log('Message from web:', messageFromWeb);
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <TouchableOpacity onPress={goBack}>
-          <Text style={styles.backButton}>Go Back</Text>
-        </TouchableOpacity>
-      </View>
-      <WebView
-        ref={webViewRef}
-        source={{ uri: 'https://sound-illegally-maggot.ngrok-free.app/end/elogin' }}
-        style={{ flex: 1 }}
-      />
-    </View>
+    <WebView
+      ref={webViewRef}
+      source={{ uri: 'https://legible-ladybird-reasonably.ngrok-free.app/end/elogin' }}
+      style={{ flex: 1, marginTop: 20 }}
+      onShouldStartLoadWithRequest={onShouldStartLoadWithRequest}
+      onMessage={(event) => {
+        alert(event.nativeEvent.data);
+      }}
+    />
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  header: {
-    padding: 10,
-    backgroundColor: 'lightgray',
-  },
-  backButton: {
-    fontSize: 18,
-    color: 'blue',
-  },
-});
+export default App;
